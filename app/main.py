9f6d0032ebe11d49f4e0a7910413322d7ded80c7
hajersmiai <hajer.smiai@gmail.com>
2025-08-20 10:08:56 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,12 +14,8 @@ def get_env_config(env: str):
 
 def page():
 
-    app_env = os.getenv("APP_ENV")
+    app_env = st.secrets.get("APP_ENV", "dev")
     title, bg = get_env_config(app_env, "dev")
-
-    app_env = os.getenv("APP_ENV", "dev")
-    title, bg = get_env_config(app_env)
- 
     st.set_page_config(page_title=title, layout="wide")
     st.markdown(
         f"""
